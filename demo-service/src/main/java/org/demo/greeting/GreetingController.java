0dbca5c368d59ee785f991d6f0e0b8dbf7e56096
Phanuphat Cholatda <remix.tk108@gmail.com>
2018-12-30 01:40:41 +0700

    add hello world

---
+++ demo-service/src/main/java/org/demo/greeting/GreetingController.java
@@ -0,0 +1,17 @@
+package org.demo.greeting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World !!";
+    }
+}
