a96bd8b2b4dc48efcf8662da600ff1c0a164f3bb
Phanuphat Cholatda <remix.tk108@gmail.com>
2019-02-22 11:17:34 +0700

    เพิ่ม overload ของ hello() ให้รับ input ได้

--- demo-service/src/main/java/org/demo/greeting/GreetingController.java
+++ demo-service/src/main/java/org/demo/greeting/GreetingController.java
@@ -2,6 +2,7 @@ package org.demo.greeting;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +14,10 @@ public class GreetingController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World !!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name + "!!";
     }
 }
